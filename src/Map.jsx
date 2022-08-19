ecd0fb2ae35a2b29835177e2c915178856ac127c
Stefan Helms <mail@estefan.de>
2022-08-19 14:25:57 +0200

    Maps working

--- src/Map.jsx
+++ src/Map.jsx
@@ -5,13 +5,17 @@ import {NightStyle} from "./NightStyle";
 
 export default function Map() {
 
+      const containerStyle = {
+            width: '100%',
+            height: '100%'
+      };
       const [mapStyle, setMapStyle] = useState([...NightStyle]);
 
       return (
           <div className="App">
                 <LoadScript
                     id="script-loader"
-                    googleMapsApiKey="AIzaSyCNReGHN6Uan9yZY4Fjh0DwKN43q--Tya8"
+                    googleMapsApiKey=""
                     language="de"
                     region="de"
                 >
@@ -25,6 +29,7 @@ export default function Map() {
                                 gestureHandling: 'greedy',
 
                           }}
+                          mapContainerStyle={containerStyle}
                           mapContainerClassName="App-map"
                           center={{lat: 53.53, lng: 9.99}}
                           zoom={12.0}
