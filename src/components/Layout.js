05135e524d335e0245ed9de08508d24713173a29
Stefan Helms <mail@estefan.de>
2022-08-19 17:46:06 +0200

    Fullscreen-Test

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,9 @@
 import '../App.css';
 import closeBox from "../images/close-box.png";
 import brightness from "../images/brightness-4.png";
+import fullscreen from "../images/fullscreen.png";
+import ReactDOM from "react-dom/client";
+
 export default function Layout({children}) {
 //Erste div kriegt das Grid
       //h1 bekommt 10%
@@ -8,9 +11,23 @@ export default function Layout({children}) {
 
 
       return (
-          <div className={"GridContainer"} >
+          <div className={"GridContainer"}>
                 <div className={"ActionChild"}>
-                      {/*      <h1 className={"AppTitle"}>X-Navigator</h1>*/}
+                      <h1 className={"AppTitle"}>X-Navigator</h1>
+
+
+                      <img
+
+                          src={fullscreen}
+                          alt={"fullscreen"}
+                          width={30}
+                          height={30}
+                          className={"Fullscreen"}
+                          onClick={(event) => {
+                                fullScreen();
+                          }}
+
+                      />
 
                       <img
 
@@ -19,6 +36,9 @@ export default function Layout({children}) {
                           width={30}
                           height={30}
                           className={"Brightness"}
+                          onClick={(event) => {
+
+                          }}
 
                       />
 
@@ -28,4 +48,9 @@ export default function Layout({children}) {
                 </div>
           </div>
       );
+}
+
+function fullScreen() {
+      const root = document.getElementById('root');
+      root.requestFullscreen();
 }
